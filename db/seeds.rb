69bc1d398a4f761a4880b1a7d1c5c77f7a617d38
lycho33 <cho.lyds@gmail.com>
2021-11-01 20:53:46 -0400

    install gems (faker)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user1 = User.create(
+    username: "cocopuffs23",
+    first_name: "Sophia",
+    last_name
+)
